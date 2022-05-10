91787e8e5af919b9571739dfd667e81023b5169f
JuanDiazAlguera99 <juancarlosdiazalguera@gmail.com>
2022-05-10 16:30:28 -0600

    Custom commands

--- cypress/integration/loginandlookupz.spec.js
+++ cypress/integration/loginandlookupz.spec.js
@@ -1,9 +1,6 @@
 describe('The Home Page', () => {
     it('successfully login and look up z', () => {
-      cy.visit('/')
-      cy.get('[data-test="username"]').type('standard_user')
-      cy.get('[data-test="password"]').type('secret_sauce')
-      cy.get('[data-test="login-button"]').click()
-      cy.get('[data-test="product_sort_container"]').select('za') 
+      cy.login()
+      cy.get('[data-test="product_sort_container"]').select('za')
     })
   })
