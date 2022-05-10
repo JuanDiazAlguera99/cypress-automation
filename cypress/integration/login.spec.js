73a806ed1b29c48270f00565b0bda12a33be26b0
JuanDiazAlguera99 <juancarlosdiazalguera@gmail.com>
2022-05-10 12:39:38 -0600

    custom commands

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,5 @@
 describe('The Home Page', () => {
     it('successfully login', () => {
-      cy.visit('/')
-      cy.get('[data-test="username"]').type('standard_user')
-      cy.get('[data-test="password"]').type('secret_sauce')
-      cy.get('[data-test="login-button"]').click()
+      cy.login()
     })
   })
-
