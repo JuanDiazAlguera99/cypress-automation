0d6b8b247c1582bb30bde4ad16c327e051e231fc
JuanDiazAlguera99 <juancarlosdiazalguera@gmail.com>
2022-05-10 17:19:02 -0600

    custo commands

--- cypress/integration/badlogin.spec.js
+++ cypress/integration/badlogin.spec.js
@@ -1,8 +1,5 @@
 describe('The Home Page', () => {
     it('error login', () => {
-      cy.visit('/')
-      cy.get('[data-test="username"]').type('problem_user')
-      cy.get('[data-test="password"]').type('secret')
-      cy.get('[data-test="login-button"]').click()
+      cy.Badlogin()
     })
   })
