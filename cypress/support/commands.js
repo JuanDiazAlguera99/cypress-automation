16fd62c2d0c64d52f84ea19c27e8eae914d80d5f
JuanDiazAlguera99 <juancarlosdiazalguera@gmail.com>
2022-05-12 15:38:50 -0600

    Custom commandscypress/integration/buyabackpack.spec.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -16,3 +16,48 @@ Cypress.Commands.add('Badlogin', (problem_user, secret)  => {
     cy.get('[data-test="login-button"]').click()
 })
  
+Cypress.Commands.add('ShoppingCartBadge', ()  => { 
+    cy.get('.shopping_cart_badge').click()
+})
+
+Cypress.Commands.add('Checkout', ()  => { 
+    cy.get('[data-test="checkout"]').click()
+})
+
+Cypress.Commands.add('Name', ()  => { 
+    cy.get('[data-test="firstName"]')
+})
+
+Cypress.Commands.add('Lastname', ()  => { 
+    cy.get('[data-test="lastName"]')
+})
+
+Cypress.Commands.add('PostalCode', ()  => { 
+    cy.get('[data-test="postalCode"]')
+})
+
+Cypress.Commands.add('Continue', ()  => { 
+    cy.get('[data-test="continue"]').click()
+})
+
+
+Cypress.Commands.add('Finish', ()  => { 
+    cy.get('[data-test="finish"]').click()
+})
+
+
+
+Cypress.Commands.add('center', ()  => { 
+    cy.scrollTo(100, 150)
+})
+
+Cypress.Commands.add('backhome', ()  => { 
+    cy.get('[data-test="back-to-products"]').click()
+})
+
+Cypress.Commands.add('buybackpack', ()  => { 
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+})
+
+
+
